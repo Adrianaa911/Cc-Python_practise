199ee17bc1ed5296f8c12191c4f09896eeaca610
Adriana <150440061+Adrianaa911@users.noreply.github.com>
2025-02-21 15:11:25 +0000

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ You are a physics teacher preparing for the upcoming semester.
 You want to provide your students with some functions that will help them calculate some fundamental physical properties.
 
                                    Tasks
-10/13 complete
+13/13 complete
 Mark the tasks as complete by checking them off
 Turn up the Temperature
 1.
@@ -77,33 +77,55 @@ Test get_work by using it on train_mass, train_acceleration, and train_distance.
 13.
 Print the string "The GE train does X Joules of work over Y meters.", with X replaced with train_work and Y replaced with train_distance.
                                    
-
 train_mass = 22680
 train_acceleration = 10
 train_distance = 100
 bomb_mass = 1
 
 
-# Write your code below: 
+#1
 def f_to_c(f_temp):
   return (f_temp - 32) * 5/9
-
+#2
 f100_in_celsius = f_to_c(100)
 #check it
 print(f100_in_celsius)
 
-
+#3
 def c_to_f(c_temp):
   return (c_temp * 9/5) + 32
-
+#4
 c0_in_fahrenheit = c_to_f(0)
 print(c0_in_fahrenheit)
 
+#5
 def get_force(mass, acceleration):
   return mass * acceleration
-
+#6
 train_force = get_force(train_mass, train_acceleration)
 print(train_force)
+#7
 print("The GE train supplies",  + train_force, "Newtons of force")
 
-def get_energy(mass, c):
+#8
+def get_energy(mass, c=3*10**8):
+
+  return mass * c** 2
+#9
+bomb_energy = get_energy(bomb_mass)
+
+#10
+print("A 1kg bomb supplies", str(bomb_energy), "Joules.")
+#or
+print("A 1kg bomb supplies", + bomb_energy, "Joules.")
+
+#11
+def get_work(mass, acceleration, distance):
+  force = get_force(mass, acceleration)
+  return force * distance
+
+#12
+train_work = get_work(train_mass, train_acceleration, train_distance)
+
+#13
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
